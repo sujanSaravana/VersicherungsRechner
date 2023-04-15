59093c775c7226ddc61876123947ab29ce4b5e1c
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-04-15 12:59:34 +0200

    Hausratsrechner enterButton, Hausratberechnung

--- rechner/src/Hausratsrechner.js
+++ rechner/src/Hausratsrechner.js
@@ -6,12 +6,33 @@ function  Hausratsrechner(){
     
    const [Quadratmeter, setQuadratmeter] = useState("");
    const [Basiswert, setBasiswert] = useState("");
+   const [Result, setResult] = useState("");
+   const [Error, setError] = useState("");
 
 
    const clear = () => {
         setQuadratmeter("");
         setBasiswert("");
+        setResult("");
    }
+
+   const change = (event) => {
+    setQuadratmeter(event.target.value);
+    };
+
+    const change1 = (event) => {
+        setBasiswert(event.target.value)
+    };
+
+   const enterButton = (event) => {
+    event.preventDefault();
+        if(Quadratmeter && Basiswert) {
+            const calculate = (Quadratmeter * Basiswert);
+            setResult(parseInt(calculate).toFixed(2));
+        }else{
+            setError("Error")
+        }
+   };
    
 
    
@@ -26,13 +47,13 @@ function  Hausratsrechner(){
                 </header>
             </div>
             <div>
-                <form className="eingaben">
+                <form className="eingaben" onSubmit={enterButton}>
                     <h2>Quadratmeter</h2>
-                    <input type="number" value={Quadratmeter}/>
+                    <input type="number" value={Quadratmeter} onChange={change}/>
                     <h2>Basiswert</h2>
-                    <input type="number" value={Basiswert} />
+                    <input type="number" value={Basiswert} onChange={change1}/>
                     <br></br>
-                    <button>Enter</button>
+                    <button type="submit">Enter</button>
                     <ClearBtn onClick={clear} />
                 </form>
             </div>
@@ -43,7 +64,7 @@ function  Hausratsrechner(){
             </div>
             <div>
                 <form>
-                    <h3>Hausratswert : </h3>
+                    <h3>Hausratswert : {Result} CHF</h3>
                 </form>
             </div>
         </div>
