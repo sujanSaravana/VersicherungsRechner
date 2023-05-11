6a2f1f757c9a27578fc9460848d144494a6623b8
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-05-11 09:08:16 +0200

    Home card infos

--- rechner/src/Home.js
+++ rechner/src/Home.js
@@ -5,7 +5,22 @@ import './App.css';
 function Home() {
     return(
         <div className="home">
+            <div class="card">
+                <div class="card-home">
+                    <h2 className="home-title">Was ist eine Hausratsversicherung?</h2>
+                    <p className="home-text">Eine Hausratsversicherung ist eine Versicherung, die den Besitz eines Haushalts gegen Verluste und Schäden absichert. Es handelt sich um eine Art von Sachversicherung, die darauf abzielt, die finanziellen Auswirkungen von Ereignissen abzufedern, die den Hausrat beeinträchtigen können. </p>
+                </div>
+            </div> 
 
+
+
+
+            <div class="card1">
+                <div class="card-home1">
+                    <h2 className="home-title">Was ist ein Schadensrechner?</h2>
+                    <p className="home-text">Ein Schadensrechner ist ein Tool, um den potenziellen finanziellen Schaden zu berechnen, der durch verschiedene Arten von Schäden an Ihrem Eigentum entstehen könnte. Indem Sie bestimmte Informationen zu Ihrem Eigentum und Ihrem Versicherungsbedarf eingeben, kann ein Schadensrechner eine Schätzung des potenziellen Schadens liefern und Ihnen helfen, eine Vorstellung davon zu bekommen.</p>
+                </div>
+            </div>
         </div>
     );
 }
